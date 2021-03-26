2d749057c3e67fcd70a06c8cf86fa583e0fadaa4
Rafael Pereira <rafael.bernardes@live.com>
2021-03-26 18:36:20 -0300

    Primeiro commit

---
+++ src/main/java/br/com/bom_destino/beans/AtualizacaoBean.java
@@ -0,0 +1,91 @@
+package br.com.bom_destino.beans;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status.Family;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+
+import br.com.bom_destino.utils.PropertiesUtil;
+
+
+@ManagedBean(name = "atualizacao")
+public class AtualizacaoBean implements Serializable {
+
+	private static final String TEXTO_SATELITE = "SATELITE";
+
+	private static final String TEXTO_IBGE = "IBGE";
+
+	private static final long serialVersionUID = 1L;
+
+	private boolean ibge;
+	private boolean satelite;
+	
+	public void atualizarDados() {
+		ResteasyClient client = new ResteasyClientBuilder().build();
+		
+		try {
+			WebTarget target = client.target(PropertiesUtil.obterURI("gateway-api")).path("dados-geograficos-stur");
+			
+			StringBuilder corpo = new StringBuilder();
+			
+			if(!ibge && !satelite) {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Informe a(s) fonte(s) desejada(s).", ""));
+				return;
+			}else if(ibge && satelite) {
+				corpo.append(TEXTO_IBGE);
+				corpo.append("|");
+				corpo.append(TEXTO_SATELITE);
+			}else if(ibge) {
+				corpo.append(TEXTO_IBGE);
+			}else if(satelite) {
+				corpo.append(TEXTO_SATELITE);
+			}
+			
+			Response response = target.request().post(Entity.entity(corpo.toString(), MediaType.APPLICATION_JSON));
+			
+			if(!Family.SUCCESSFUL.equals(response.getStatusInfo().getFamily())) {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falha ao atualizar dados geográficos.", ""));
+			}else {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Dados geográficos atualizados com sucesso", ""));
+				
+				ibge = false;
+				satelite = false;
+			}
+			
+		} catch (IllegalArgumentException e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falha ao atualizar dados geográficos", e.getMessage()));
+		} catch (NullPointerException e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falha ao atualizar dados geográficos", e.getMessage()));
+		} catch (IOException e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falha ao atualizar dados geográficos", e.getMessage()));
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falha ao atualizar dados geográficos", e.getMessage()));
+		}
+	}
+
+	public boolean isIbge() {
+		return ibge;
+	}
+
+	public void setIbge(boolean ibge) {
+		this.ibge = ibge;
+	}
+
+	public boolean isSatelite() {
+		return satelite;
+	}
+
+	public void setSatelite(boolean satelite) {
+		this.satelite = satelite;
+	}
+}
---
+++ src/main/java/br/com/bom_destino/utils/PropertiesUtil.java
@@ -0,0 +1,24 @@
+package br.com.bom_destino.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+public class PropertiesUtil {
+	
+	public static String obterURI(String nomeServico) throws IOException {
+		final File services = new File(System.getProperty("jboss.home.dir") + "/properties/services.properties");
+		
+		Properties properties = new Properties();
+		
+		FileInputStream fis = new FileInputStream(services);
+		
+		properties.load(new InputStreamReader(fis));
+		
+		fis.close();
+		
+		return properties.get(nomeServico).toString();
+	}
+}
